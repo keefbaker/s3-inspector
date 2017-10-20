93866e1553784cc2d4717b0dd35f0da8dc93d633
Keith Baker <keith.baker@checkit.net>
2017-10-20 09:00:04 +0100

    Made it fit my taste more, removed option to type in keys, removed verbose auth errors at the end.

--- s3inspector.py
+++ s3inspector.py
@@ -1,11 +1,67 @@
-import os
 import re
 import sys
+import boto3
+import botocore
+import requests
+import termcolor
 
 from collections import defaultdict
 
+def grantchecker(grant, grants, explained, groups_to_check):
+    permissions = grants[grant]
+    perm_to_print = [explained[perm]
+                     for perm in permissions]
+    termcolor.cprint('Permission: {} by {}'.format(
+        termcolor.colored(
+            ' & '.join(perm_to_print), 'red'),
+        termcolor.colored(groups_to_check[grant], 'red')))
 
-def check_acl(acl):
+def bucketcheck(bucket, bucketcount,
+                s3_client, explained):
+    groups_to_check = {
+        'http://acs.amazonaws.com/groups/global/AllUsers': 'Everyone',
+        'http://acs.amazonaws.com/groups/global/AuthenticatedUsers': 'Authenticated AWS users'
+    }
+    seperator = '-' * 40
+    location = get_location(bucket.name, s3_client)
+    print(seperator)
+    acl = bucket.Acl()
+    public, grants = check_acl(acl, groups_to_check)
+
+    if public:
+        bucket_line = termcolor.colored(
+            bucket.name, 'blue', attrs=['bold'])
+        public_ind = termcolor.colored(
+            'PUBLIC!', 'red', attrs=['bold'])
+        termcolor.cprint('Bucket {}: {}'.format(
+            bucket_line, public_ind))
+        print('Location: {}'.format(location))
+        if grants:
+            for grant in grants:
+                grantchecker(grant, grants, explained, groups_to_check)
+        urls = scan_bucket_urls(bucket.name)
+        print('URLs:')
+        if urls:
+            print('\n'.join(urls))
+        else:
+            print('Nothing found')
+    else:
+        bucket_line = termcolor.colored(
+            bucket.name, 'blue', attrs=['bold'])
+        public_ind = termcolor.colored(
+            'Not public', 'green', attrs=['bold'])
+        termcolor.cprint('Bucket {}: {}'.format(
+            bucket_line, public_ind))
+        print('Location: {}'.format(location))
+    bucketcount += 1
+    return bucketcount
+
+
+def bucketlist():
+    s3 = boto3.resource('s3')
+    return s3.buckets.all(), boto3.client('s3')
+
+def check_acl(acl, groups_to_check):
     dangerous_grants = defaultdict(list)
     for grant in acl.grants:
         grantee = grant['Grantee']
@@ -15,24 +71,12 @@ def check_acl(acl):
     return public_indicator, dangerous_grants
 
 
-def get_location(bucket_name):
+def get_location(bucket_name, s3_client):
     loc = s3_client.get_bucket_location(
         Bucket=bucket_name)['LocationConstraint']
     if loc is None:
         loc = 'None(probably North Virginia)'
     return loc
-
-
-def install_and_import(pkg):
-    import importlib
-    try:
-        importlib.import_module(pkg)
-    except ImportError:
-        import pip
-        pip.main(['install', pkg])
-    finally:
-        globals()[pkg] = importlib.import_module(pkg)
-
 
 def scan_bucket_urls(bucket_name):
     domain = 's3.amazonaws.com'
@@ -49,14 +93,7 @@ def scan_bucket_urls(bucket_name):
             access_urls.append(url)
     return access_urls
 
-
-if __name__ == '__main__':
-    if sys.version[0] == "3":
-        raw_input = input
-    packages = ['boto3', 'botocore', 'termcolor', 'requests']
-    for package in packages:
-        install_and_import(package)
-    SEP = '-' * 40
+def bucketloop():
     explained = {
         'READ': 'readable',
         'WRITE': 'writable',
@@ -64,92 +101,18 @@ if __name__ == '__main__':
         'WRITE_ACP': 'permissions writeable',
         'FULL_CONTROL': 'Full Control'
     }
-    groups_to_check = {
-        'http://acs.amazonaws.com/groups/global/AllUsers': 'Everyone',
-        'http://acs.amazonaws.com/groups/global/AuthenticatedUsers': 'Authenticated AWS users'
-    }
-    if (os.path.exists('/home/{}/.aws/credentials'.format(os.getlogin())) or
-            os.path.exists('/home/{}/.aws/config'.format(os.getlogin()))):
-        s3 = boto3.resource('s3')
-        s3_client = boto3.client('s3')
-    else:
-        access_key = raw_input('Enter your AWS access key ID: ')
-        secret_key = raw_input('Enter your AWS secret key: ')
-        s3 = boto3.resource('s3', aws_access_key_id=access_key,
-                            aws_secret_access_key=secret_key)
-        s3_client = boto3.client('s3', aws_access_key_id=access_key,
-                                 aws_secret_access_key=secret_key)
-    bucket_list = []
-    buckets = s3.buckets.all()
+
+    bucketcount = 0
+    buckets, s3_client = bucketlist()
+    for bucket in buckets:
+        bucketcount = bucketcheck(bucket, bucketcount,
+                                  s3_client, explained)
+    if not bucketcount:
+        print('No buckets found')
+        termcolor.cprint(termcolor.colored('You are safe', 'green'))
+
+if __name__ == '__main__':
     try:
-        bucketcount = 0
-        for bucket in buckets:
-            location = get_location(bucket.name)
-            print(SEP)
-            acl = bucket.Acl()
-            public, grants = check_acl(acl)
-
-            if public:
-                bucket_line = termcolor.colored(
-                    bucket.name, 'blue', attrs=['bold'])
-                public_ind = termcolor.colored(
-                    'PUBLIC!', 'red', attrs=['bold'])
-                termcolor.cprint('Bucket {}: {}'.format(
-                    bucket_line, public_ind))
-                print('Location: {}'.format(location))
-                if grants:
-                    for grant in grants:
-                        permissions = grants[grant]
-                        perm_to_print = [explained[perm]
-                                         for perm in permissions]
-                        termcolor.cprint('Permission: {} by {}'.format(
-                            termcolor.colored(
-                                ' & '.join(perm_to_print), 'red'),
-                            termcolor.colored(groups_to_check[grant], 'red')))
-                urls = scan_bucket_urls(bucket.name)
-                print('URLs:')
-                if urls:
-                    print('\n'.join(urls))
-                else:
-                    print('Nothing found')
-            else:
-                bucket_line = termcolor.colored(
-                    bucket.name, 'blue', attrs=['bold'])
-                public_ind = termcolor.colored(
-                    'Not public', 'green', attrs=['bold'])
-                termcolor.cprint('Bucket {}: {}'.format(
-                    bucket_line, public_ind))
-                print('Location: {}'.format(location))
-            bucketcount += 1
-        if not bucketcount:
-            print('No buckets found')
-            termcolor.cprint(termcolor.colored('You are safe', 'green'))
-    except botocore.exceptions.ClientError as e:
-        msg = str(e)
-        if 'InvalidAccessKeyId' in msg and 'does not exist' in msg:
-            print('The Access Key ID you provided does not exist')
-            print('Please, make sure you give me the right credentials')
-        elif 'SignatureDoesNotMatch' in msg:
-            print('The Secret Access Key you provided is incorrect')
-            print('Please, make sure you give me the right credentials')
-        elif 'AccessDenied' in msg:
-            print('''Access Denied
-I need permission to access S3
-Check if the IAM user at least has AmazonS3ReadOnlyAccess policy attached
-
-To find the list of policies attached to your user, perform these steps:
-1. Go to IAM (https://console.aws.amazon.com/iam/home)
-2. Click "Users" on the left hand side menu
-3. Click the user, whose credentials you give me
-4. Here it is
-''')
-        else:
-            print('''{}
-Check your credentials in ~/.aws/credentials file
-
-The user also has to have programmatic access enabled
-If you didn't enable it(when you created the account), then:
-1. Click the user
-2. Go to "Security Credentials" tab
-3. Click "Create Access key"
-4. Use these credentials'''.format(msg))
+        bucketloop()
+    except botocore.exceptions.ClientError as error:
+        print(str(error))
